c4db3423741e3707ed5f344b9be86df1cb6c28ec
Ivan Kravtsov <kravcneger@gmail.com>
2015-03-24 23:48:31 +0400

    Updates spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,10 @@
+require 'rubygems'
+require 'bundler/setup'
+# our gem
 require 'sm_archivator'
+
+RSpec.configure do |config|
+  config.failure_color = :red
+  config.tty = true
+  config.color = true
+end

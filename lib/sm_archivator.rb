e5170bae7ff66a12ae8b7ada10b96cdfeb4e323f
Ivan Kravtsov <kravcneger@gmail.com>
2015-03-31 21:08:00 +0400

    Remove unnecessary files

--- lib/sm_archivator.rb
+++ lib/sm_archivator.rb
@@ -9,6 +9,29 @@ require "sm_archivator/utf8"
 
 module SmArchivator
   
-  def self.read_trie(stream)
+  
+  def self.compress(str)
+  end
+  
+  def self.expand(str)
+  end
+
+  private
+
+  def size_archived_data(str)
+    # How much bytes was allocated to record the number of characters 
+    # without tree
+    first_byte = @stream[0, 8].to_i(2)
+    
+    # Contains number of missing bits
+    # stream - 01100_000 closed byte 00000011 = 3 bit
+    closed_byte = str[-8, 8]
+
+
+    @stream[8, first_byte * 8].to_i(2)
+  end
+  
+  def set_size_of_archived_data
+    
   end
 end

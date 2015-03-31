e1ac0074a1ea4667a48d44483d8fcf78e1634b8a
Ivan Kravtsov <kravcneger@gmail.com>
2015-03-31 19:08:19 +0400

    Renaming from binary_reader to binary_stream

--- lib/sm_archivator.rb
+++ lib/sm_archivator.rb
@@ -4,9 +4,11 @@ require "sm_archivator/pq"
 require "sm_archivator/node"
 require "sm_archivator/trie_builder"
 require "sm_archivator/table_builder"
-require "sm_archivator/binary_reader"
+require "sm_archivator/binary_stream"
 require "sm_archivator/utf8"
 
 module SmArchivator
-  # Your code goes here...
+  
+  def self.read_trie(stream)
+  end
 end

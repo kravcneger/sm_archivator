3497e67d3a3e57ce149656b9444c0a613e63f95b
Ivan Kravtsov <kravcneger@gmail.com>
2015-04-02 20:04:07 +0400

    Сompletes the module

--- lib/sm_archivator.rb
+++ lib/sm_archivator.rb
@@ -8,30 +8,71 @@ require "sm_archivator/binary_stream"
 require "sm_archivator/utf8"
 
 module SmArchivator
-  
-  
+
   def self.compress(str)
+    @@binary_stream.clear_stream!
+
+    tree = TrieBuilder.build(str).min
+    self::write_tree(tree)
+
+    table = TableBuilder.build(tree)
+
+    str.each_char do |ch|
+      table[ch].each_char do |bit|
+        @@binary_stream.write_bit!(bit)
+      end
+    end
+
+    @@binary_stream.close_stream!
+    @@binary_stream.all_stream
   end
-  
+
+
   def self.expand(str)
+    @@binary_stream.set_stream!(str)
+    @@binary_stream.remove_last_bits!
+
+    result = ''
+    root = self::read_trie
+
+    while !@@binary_stream.in_end?
+      x = root
+
+      while !x.is_leaf?
+        if @@binary_stream.read_bit == '1'
+          x = x.right
+        else
+          x = x.left
+        end
+      end
+      result += x.ch
+    end
+    result
   end
 
   private
 
-  def size_archived_data(str)
-    # How much bytes was allocated to record the number of characters 
-    # without tree
-    first_byte = @stream[0, 8].to_i(2)
-    
-    # Contains number of missing bits
-    # stream - 01100_000 closed byte 00000011 = 3 bit
-    closed_byte = str[-8, 8]
+  @@binary_stream = BinaryStream.new
 
+  def self.write_tree(node)
+    return unless node
 
-    @stream[8, first_byte * 8].to_i(2)
+    if(node.is_leaf?)
+      @@binary_stream.write_bit!('1')
+      @@binary_stream.write_char!(node.ch)
+      return
+    end
+    @@binary_stream.write_bit!('0')
+
+    self::write_tree(node.left)
+    self::write_tree(node.right)
   end
-  
-  def set_size_of_archived_data
-    
+
+  def self.read_trie
+    if @@binary_stream.read_bit == '1'
+      return Node.new(@@binary_stream.read_utf8_char, 0, nil, nil)
+    end
+    return Node.new('0', 0, self::read_trie, self::read_trie)
   end
+
 end
